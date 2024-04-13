dda3911f8bf1368cfcc13d2f32e96441055a7450
Charles Derber <admchader@testhsorhf.onmicrosoft.com>
2024-04-13 23:45:35 +0200

    keyvaut module

---
+++ resources/resource_keyvault/outputs.tf
@@ -0,0 +1,19 @@
+output "private_endpoints" {
+  description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
+  value       = azurerm_private_endpoint.this
+}
+
+output "resource" {
+  description = "The Key Vault resource."
+  value       = azurerm_key_vault.this
+}
+
+output "resource_keys" {
+  description = "A map of key objects. The map key is the supplied input to var.keys. The map value is the entire azurerm_key_vault_key resource."
+  value       = azurerm_key_vault_key.this
+}
+
+output "resource_secrets" {
+  description = "A map of secret objects. The map key is the supplied input to var.secrets. The map value is the entire azurerm_key_vault_secret resource."
+  value       = azurerm_key_vault_secret.this
+}
